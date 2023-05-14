68929263e0ceebc9fcb3fd38fe4364f1bd5decc0
Iagocp <114144454+Iagocp@users.noreply.github.com>
2023-05-14 14:02:03 +0200

    Add files via upload

---
+++ Herencia8_Ciclismo/Equipo.java
@@ -0,0 +1,85 @@
+package Herencia8_Ciclismo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Equipo {
+	protected String nombre;
+	protected double totalTiempo;
+	String pais;
+	protected ArrayList<Ciclista> listaCiclistas;
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	Equipo(String nombre, String pais){
+		this.nombre=nombre;
+		this.pais=pais;
+		totalTiempo=0;
+		listaCiclistas = new ArrayList();
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	@SuppressWarnings("unused")
+	public void calcularTotalTiempo() {
+		double res=0;
+		for (Ciclista ciclista : listaCiclistas) {
+			this.totalTiempo+=ciclista.getTiempoAcumulado();
+		}
+		System.out.println("Tiempo acumulado es "+ this.totalTiempo);
+	}
+	
+	public void añadirCicilista(Ciclista ciclista) {
+		this.listaCiclistas.add(ciclista);
+	}
+	public void listarEquipos(){
+		for (Ciclista ciclista : listaCiclistas) {
+			ciclista.toString();
+		}
+	}
+	public void buscarCiclista() {
+		@SuppressWarnings("resource")
+		Scanner src= new Scanner(System.in);
+		System.out.println("Introduce el nombre del ciclista");
+		String nom= src.next();
+		boolean algo = true;
+		for (Ciclista ciclista : listaCiclistas) {
+			if(nom.equals(ciclista.getNombre())) {
+				ciclista.toString();
+				algo=false;
+			}	
+		}
+		if(algo) {
+			System.out.println("No existe");
+		}
+	}
+	public void ordernarPosiciones() {
+		Collections.sort(listaCiclistas, new Comparator<Ciclista>() {
+            @Override
+            public int compare(Ciclista p1, Ciclista p2) {
+                return p1.getTiempoAcumulado() - p2.getTiempoAcumulado();
+            }
+        });
+	}
+	
+	public void ponerPosicion() {
+		for (int i=0; i<listaCiclistas.size(); i++) {
+			listaCiclistas.get(i).setPosicionGeneral(i+1);
+		}
+	}
+	
+	
+	
+}
