8ff7c0f5983c7a3989b2aab55a2d48a3420b0563
Iagocp <114144454+Iagocp@users.noreply.github.com>
2023-05-18 13:55:24 +0200

    Add files via upload

---
+++ LecturaArchivo/Asignatura.java
@@ -0,0 +1,64 @@
+package LecturaArchivo;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class Asignatura implements Serializable{
+	int codigo;
+	String nombre;
+	int credits;
+	
+	Asignatura(int codigo,String nombre, int creditos){
+		
+		this.codigo=codigo;
+		this.nombre=nombre;
+		this.credits=creditos;
+	}
+
+	void imprimir() {
+		System.out.println("Codigo de la clase:" + codigo);
+		System.out.println("Nombre de la clase:" + nombre);
+		System.out.println("Creditos de la clase:" + credits);
+	}
+	void escribirAsignatura() throws Exception{
+		try {
+			FileOutputStream archivo = new FileOutputStream("Asignatura.dat");
+			ObjectOutputStream salida= new ObjectOutputStream(archivo);
+			salida.writeObject(this);
+			salida.close();
+		}catch(IOException e) {
+			System.out.println("No se pudo escribir el archivo");
+			
+		}
+	}
+	void leerAsignatura() throws Exception {
+		try {
+			FileInputStream archivo = new FileInputStream("Asignatura.dat");
+			ObjectInputStream entrada= new ObjectInputStream(archivo);
+			Asignatura asignatura= (Asignatura) entrada.readObject();
+			asignatura.imprimir();
+			entrada.close();
+		}catch(FileNotFoundException e) {
+			System.out.println("No se pudo escribir el archivo");
+		}catch(IOException e) {
+			System.out.println("No se pudo escribir el archivo");
+			
+		}catch(Exception e) {
+			System.out.println("No se pudo escribir el archivo");
+			
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Asignatura [codigo=" + codigo + ", nombre=" + nombre + ", credits=" + credits + "]";
+	}
+	
+	
+}
---
+++ LecturaArchivo/TestLista.java
@@ -0,0 +1,46 @@
+package LecturaArchivo;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class TestLista {
+	@SuppressWarnings({ "unused", "unchecked" })
+	public static void main(String[] args) throws Exception {
+		
+		ArrayList<Asignatura> arrayEscribir= new ArrayList<Asignatura>();
+		ArrayList<Asignatura> arrayLeer= new ArrayList<Asignatura>();
+		
+		System.out.println("Asignaturas que se van a escribir en el fichero: ");
+		for(int i=0;i<5;i++) {
+			Asignatura newObject = new Asignatura(i,"cadena",5*i);
+			System.out.println("ArrayEscribir[" + i + "] = " + arrayEscribir.get(i).toString());
+		}try {
+			System.out.println("Guardando Arraylist en el fichero objectos.dat ...");
+			ObjectOutputStream escribiendoFichero = new ObjectOutputStream(new  FileOutputStream("objectos.dat"));
+			escribiendoFichero.writeObject(arrayEscribir);
+			escribiendoFichero.close();
+			System.out.println("ok");
+			
+			System.out.println("Leyendo Arraylist del fichero objetos.dat...");
+			ObjectInputStream leyendoFichero = new ObjectInputStream(new FileInputStream("objetos.dat")); 
+			
+			arrayLeer= (ArrayList <Asignatura>) leyendoFichero.readObject();
+			leyendoFichero.close();
+			
+			System.out.println("Leido Correctamente");
+			System.out.println("Los datos son los siguientes: ");
+			
+			for(int i = 0;i< arrayLeer.size();i++) {
+				System.out.println("Array leer" + i + "] = " + arrayLeer.get(i).toString());
+			}
+		
+	
+}catch(IOException e) {
+	e.printStackTrace();
+}
+}
+}
